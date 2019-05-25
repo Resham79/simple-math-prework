06d9142a26bcc81466192346848b7f5df4431288
Resham Arora <resham.arora@gmail.com>
2019-05-25 22:52:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -17,9 +17,15 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
+  num1 = 4 
+  num2 = 30
+  return num1*num2
 end
 
 def modulo(num1, num2)
+  num1 = 34
+  num2 = 5 
+  return num1%num2
 end
 
 def square_root(num)
